9f186f7a393f9d455fe4ff24a1a3a1b2209563d9
nuubprogrammer <silje.e.wilhelmsen@gmail.com>
2025-01-08 15:25:30 +0100

    Add load data

--- Visualization/create_networkplot_avg_log2fc.R
+++ Visualization/create_networkplot_avg_log2fc.R
@@ -6,6 +6,15 @@
 
 # Prepare data 
 
+# Create and save plot
+##################################
+# Function to create plot
+create_network <- function() {
+  
+}
+
 # Create plot
+network_plot <- create_network(heatmap_matrix_numeric, go_association_vector)
 
 # Save plot
+ggsave(file.path(output_dir_plot, paste(go_processes_of_interest_for_heatmap, "_heatmap.png", sep = "")), plot = combined_plot, width = 10, height = 7)
